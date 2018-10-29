dcf3f046d8f87c77bbedf751963c0f1856631c2a
rlftmdtp <rlfcksdlf@naver.com>
2018-10-29 16:53:19 +0900

    Hello.java modified on GitHub

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ class Hello
 	public static void main(String[] args)
 	{
 		// For command line git tutorial
-		// Local repository modification
+		// GitHub modification
 		System.out.println("Hellow java");
 		System.out.println("Tell your world");
 		System.out.println("Tell his wordl");
